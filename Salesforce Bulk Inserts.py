8c3717f3ab0c2958ae514f4521c6ec0b4475799f
struwwelpete <39143062+struwwelpete@users.noreply.github.com>
2018-06-07 22:49:00 -0700

    Update Salesforce Bulk Inserts.py

--- Salesforce Bulk Inserts.py
+++ Salesforce Bulk Inserts.py
@@ -8,25 +8,96 @@ Created on Mon Apr  9 22:15:09 2018
 from salesforce_bulk import SalesforceBulk, CsvDictsAdapter
 import unicodecsv
 import time
+import pandas as pd
 from pandas import DataFrame as df
 
-prod_bulk = SalesforceBulk(username='admin@havensconsulting.net',password='91dU9hsKZdkz',security_token='ATLclvPX1UFxT05UZMIHrAkM')
-test_bulk = SalesforceBulk(username='michael@havensconsulting.net.mikesde',password='P@tersin1',sandbox=True, security_token='K7FjFIUW3KuMXKFthijArNDFP')
 
-test_contacts = pd.read_csv(r"C:\Users\micha\Documents\NWFB\test_examples\contactsFile3.csv")
+#prod_bulk = SalesforceBulk(username='admin@havensconsulting.net',password='91dU9hsKZdkz',security_token='ATLclvPX1UFxT05UZMIHrAkM')
+#test_bulk = SalesforceBulk(username='michael@havensconsulting.net.mikesde',password='P@tersin1',sandbox=True, security_token='K7FjFIUW3KuMXKFthijArNDFP')#
+#account = pd.read_csv(r"C:\Users\micha\Desktop\accounts_for_sandbox.csv")
 
-for column in test_contacts:
-    for index, item in test_contacts.iterrows():
-        test_contacts.loc[index,column] = str(test_contacts.loc[index,column]).replace('nan','')
 
-job = test_bulk.create_insert_job("Contact", contentType='CSV', concurrency='Serial')
+def instance_login(sandbox):
+    if sandbox == True:
+        creds = SalesforceBulk(username='michael@havensconsulting.net.mikesde',password='P@tersin1',sandbox=True, security_token='K7FjFIUW3KuMXKFthijArNDFP')
+        print("Sandbox Login Successful!")
+    elif sandbox == False:
+        creds = SalesforceBulk(username='admin@havensconsulting.net',password='91dU9hsKZdkz',security_token='ATLclvPX1UFxT05UZMIHrAkM')
+        print("Product Login Successful!")
+    else: 
+        print("Please Input True or False")
+    return creds
 
-contacts = [dict(test_contacts) for idx in test_contacts]
-contacts = contacts
-#contacts = [dict(Name="Contact%d" % idx) for idx in range(5)]
-csv_iter = CsvDictsAdapter(iter(test_contacts))
-batch = test_bulk.post_batch(job, csv_iter)
-test_bulk.batch_status()
-test_bulk.wait_for_batch(job, batch)
-test_bulk.close_job(job)
-print("Done. Accounts uploaded.")
+
+#test_contacts = pd.read_csv(r"C:\Users\micha\Documents\NWFB\test_examples\contactsFile3.csv")
+#test2 = test_contacts[['FirstName','LastName','AccountId','Caseworker_Name__c']]
+
+def cleaning_pandas(dataframe):
+    ''' removes all nans'''
+    for column in dataframe:
+        for index, item in dataframe.iterrows():
+            dataframe.loc[index,column] = str(dataframe.loc[index,column]).replace('nan','')
+    return dataframe
+
+
+def insert_job(creds, sf_object, dataset):
+    results = 0
+    wait = 15
+    #try:
+    print('ya made it here kid')
+    job = creds.create_insert_job(sf_object, contentType='JSON', concurrency='Serial')
+    cont_dict = dataset.to_json(orient = 'records')
+    print('oi keep going')
+    csv_iter = iter(cont_dict)
+    batch = creds.post_batch(job, csv_iter)
+    print('i member atst')
+    print(creds.batch_status(batch))
+    print('error there')
+#        for i in range(wait):
+ #           time.sleep(1)
+  #          print(creds.batch_status(batch))
+#        results = creds.batch_status(batch)
+#        time.sleep(10)
+    print('error here')
+    creds.wait_for_batch(job, batch)
+    results = creds.batch_status(batch)
+    print('maybe error here')
+    creds.close_job(job)
+    print(sf_object + "s " + "Insert Job Complete")
+    return results
+#        return results
+#    except:
+#        print("error")
+ #       creds.close_job(job)
+  #  if results == 0:
+   #     pass
+   # else:
+    #    return results
+    
+
+ 
+
+#login = instance_login(True)
+#contacts = cleaning_pandas(account)
+#results = insert_job(login, "Account",contacts)
+
+#columns2 = []
+#values = []
+#for item in results.keys():
+#    columns2.insert(0,item)
+#for item in results.values():
+#    print(item)
+#    values.insert(0,item)
+    
+#results2 = df(columns = columns2, data = values)
+    
+#insert_job(login,"Contact",contacts)
+
+#job = login.create_insert_job("Contact", contentType='JSON', concurrency='Serial')
+#cont_dict = contacts.to_json(orient = 'records')
+#csv_iter = iter(cont_dict)
+#batch = login.post_batch(job, csv_iter)
+#print(login.batch_status(batch))
+#login.wait_for_batch(job, batch)
+#login.close_job(job)
+
